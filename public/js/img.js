8f5c50965ce43d24831c2a1c359d2b10d04aec5a
Mthobisi-za <mthobisimtho333@gmail.com>
2021-12-25 23:24:25 +0200

    fixed

--- public/js/img.js
+++ public/js/img.js
@@ -8,7 +8,7 @@ function makeElem(src) {
         img
     }
 }
-var div = document.querySelector('.rightbox');
+var div = document.querySelector('.boxedright');
 
 var fileinput = document.querySelector('.file');
 fileinput.addEventListener('change', () => {
@@ -38,3 +38,24 @@ function clear() {
 
 var button = document.querySelector('.btnclear');
 button.addEventListener('click', clear);
+
+
+
+
+var textarea = document.querySelector('#tags');
+var divtag = document.querySelector('.textt');
+var inp = document.querySelector('.inp');
+
+function checkTags() {
+    if (textarea.value.includes(',')) {
+        var data = textarea.value.split(',');
+        document.querySelector('#tags').value = ' ';
+        var p = document.createElement('p');
+        p.classList.add('ptag');
+        p.innerHTML = data[0];
+        divtag.appendChild(p);
+        var dynamic = divtag.textContent;
+        inp.value = dynamic;
+        console.log(inp);
+    }
+}
